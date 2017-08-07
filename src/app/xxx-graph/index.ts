e81ca84fa4ef39166cb3c5997090c450b2f0bbcb
Alex Pikalov <alex.pikalov.khar@gmail.com>
2017-08-07 22:56:43 +0200

    Remove old graph folder

--- src/app/xxx-graph/index.ts
+++
@@ -1,5 +0,0 @@
-export { FetchGraphAction, ResetGraphAction } from './graph.actions';
-export { GraphEffects } from './graph.effects';
-export { GraphService } from './graph.service';
-export { Graph } from './graph';
-export { graph } from './graph.reducer';
